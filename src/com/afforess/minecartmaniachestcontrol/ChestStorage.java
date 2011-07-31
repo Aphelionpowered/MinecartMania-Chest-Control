e33a1d623fdd783ef33ad19e8befbcd731ae21be
N3X15 <nexis@7chan.org>
2011-07-30 22:29:00 -0700

    Fix compress, add snow, wool, and diamond to compressor

--- src/com/afforess/minecartmaniachestcontrol/ChestStorage.java
+++ src/com/afforess/minecartmaniachestcontrol/ChestStorage.java
@@ -18,143 +18,159 @@ import com.afforess.minecartmaniacore.utils.MinecartUtils;
 import com.afforess.minecartmaniacore.utils.SignUtils;
 
 public abstract class ChestStorage {
-	
-	public static Location getSpawnLocation(MinecartManiaChest chest) {
-		Block center = chest.getLocation().getBlock();
-		Location result = getAdjacentTrack(center);
-		if (result == null && chest.getNeighborChest() != null) {
-			result = getAdjacentTrack(chest.getNeighborChest().getLocation().getBlock());
-		}
-		return result;
-	}
-	
-	private static Location getAdjacentTrack(Block center) {
-		if (MinecartUtils.isTrack(center.getRelative(-1, 0, 0))) {
-			return center.getRelative(-1, 0, 0).getLocation();
-		}
-		else if (MinecartUtils.isTrack(center.getRelative(-1, -1, 0)) && MinecartUtils.isSlopedTrack(center.getRelative(-1, -1, 0))) {
-			return center.getRelative(-1, 0, 0).getLocation();
-		}
-		if (MinecartUtils.isTrack(center.getRelative(0, 0, -1))) {
-			return center.getRelative(0, 0, -1).getLocation();
-		}
-		else if (MinecartUtils.isTrack(center.getRelative(0, -1, -1)) && MinecartUtils.isSlopedTrack(center.getRelative(0, -1, -1))) {
-			return center.getRelative(0, 0, -1).getLocation();
-		}
-		if (MinecartUtils.isTrack(center.getRelative(1, 0, 0))) {
-			return center.getRelative(1, 0, 0).getLocation();
-		}
-		else if (MinecartUtils.isTrack(center.getRelative(1, -1, 0)) && MinecartUtils.isSlopedTrack(center.getRelative(1, -1, 0))) {
-			return center.getRelative(1, 0, 0).getLocation();
-		}
-		if (MinecartUtils.isTrack(center.getRelative(0, 0, 1))) {
-			return center.getRelative(0, 0, 1).getLocation();
-		}
-		else if (MinecartUtils.isTrack(center.getRelative(0, -1, 1)) && MinecartUtils.isSlopedTrack(center.getRelative(0, -1, 1))) {
-			return center.getRelative(0, 0, 1).getLocation();
-		}
-		return null;
-	}
-	
-	public static boolean doMinecartCollection(MinecartManiaMinecart minecart) {
-		if (minecart.getBlockTypeAhead() != null) {
-			if (minecart.getBlockTypeAhead().getType().getId() == Item.CHEST.getId()) {
-				MinecartManiaChest chest = MinecartManiaWorld.getMinecartManiaChest((Chest)minecart.getBlockTypeAhead().getState());
-				
-				if (SignCommands.isNoCollection(chest)) {
-					return false;
-				}
-				
-				if (minecart instanceof MinecartManiaStorageCart) {
-					MinecartManiaStorageCart storageCart = (MinecartManiaStorageCart)minecart;
-					boolean failed = false;
-					for (ItemStack item : storageCart.getInventory().getContents()) {
-						if (!chest.addItem(item)) {
-							failed = true;
-							break;
-						}
-					}
-					if (!failed) {
-						storageCart.getInventory().clear();
-					}
-				}
-				if (chest.addItem(minecart.getType().getId())) {
-					
-					minecart.kill(false);
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	public static boolean doCollectParallel(MinecartManiaMinecart minecart) {
-		ArrayList<Block> blockList = minecart.getParallelBlocks();
-		for (Block block : blockList) {
-			if (block.getState() instanceof Chest) {
-				MinecartManiaChest chest = MinecartManiaWorld.getMinecartManiaChest((Chest)block.getState());
-				ArrayList<Sign> signList = SignUtils.getAdjacentMinecartManiaSignList(chest.getLocation(), 1);
-				for (Sign sign : signList) {
-					for (int i = 0; i < sign.getNumLines(); i++) {
-						if (sign.getLine(i).toLowerCase().contains("parallel")) {
-							sign.setLine(i, "[Parallel]");
-							if (!minecart.isMovingAway(block.getLocation())) {
-								if (chest.addItem(minecart.getType().getId())) {
-									minecart.kill(false);
-									return true;
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		return false;
-	}
-
-	public static void doItemCompression(MinecartManiaStorageCart minecart) {
-		HashSet<Block> blockList = minecart.getAdjacentBlocks(minecart.getRange());
-		for (Block block : blockList) {
-			if (block.getTypeId() == Item.WORKBENCH.getId()) {
-				ArrayList<Sign> signList = SignUtils.getAdjacentMinecartManiaSignList(block.getLocation(), 2);
-				for (Sign sign : signList) {
-					for (int i = 0; i < sign.getNumLines(); i++) {
-						if (sign.getLine(i).toLowerCase().contains("compress items")) { 
-							sign.setLine(i, "[Compress Items]");
-							//TODO handling for custom recipies?
-							Item[][] compressable = { {Item.IRON_INGOT, Item.GOLD_INGOT, Item.LAPIS_LAZULI}, {Item.IRON_BLOCK , Item.GOLD_BLOCK, Item.LAPIS_BLOCK} };
-							int n = 0;
-							for (Item m : compressable[0]) {
-								int amt = 0;
-								int slot = 0;
-								for (ItemStack item : minecart.getContents()) {
-									if (item != null && m.equals(Item.getItem(item))) {
-										amt += item.getAmount();
-										minecart.setItem(slot, null);
-									}
-									slot++;
-								}
-								int compressedAmt = amt / 9;
-								int left = amt % 9;
-								while (compressedAmt > 0) {
-									minecart.addItem(compressable[1][n].getId(), Math.min(64, compressedAmt));
-									compressedAmt -= Math.min(64, compressedAmt);
-								}
-								if (left > 0) {
-									ItemStack item = compressable[0][n].toItemStack();
-									item.setAmount(left);
-									minecart.addItem(item);
-								}
-								
-								n++;
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-/*	
+    
+    public static Location getSpawnLocation(MinecartManiaChest chest) {
+        Block center = chest.getLocation().getBlock();
+        Location result = getAdjacentTrack(center);
+        if (result == null && chest.getNeighborChest() != null) {
+            result = getAdjacentTrack(chest.getNeighborChest().getLocation().getBlock());
+        }
+        return result;
+    }
+    
+    private static Location getAdjacentTrack(Block center) {
+        if (MinecartUtils.isTrack(center.getRelative(-1, 0, 0))) {
+            return center.getRelative(-1, 0, 0).getLocation();
+        }
+        else if (MinecartUtils.isTrack(center.getRelative(-1, -1, 0)) && MinecartUtils.isSlopedTrack(center.getRelative(-1, -1, 0))) {
+            return center.getRelative(-1, 0, 0).getLocation();
+        }
+        if (MinecartUtils.isTrack(center.getRelative(0, 0, -1))) {
+            return center.getRelative(0, 0, -1).getLocation();
+        }
+        else if (MinecartUtils.isTrack(center.getRelative(0, -1, -1)) && MinecartUtils.isSlopedTrack(center.getRelative(0, -1, -1))) {
+            return center.getRelative(0, 0, -1).getLocation();
+        }
+        if (MinecartUtils.isTrack(center.getRelative(1, 0, 0))) {
+            return center.getRelative(1, 0, 0).getLocation();
+        }
+        else if (MinecartUtils.isTrack(center.getRelative(1, -1, 0)) && MinecartUtils.isSlopedTrack(center.getRelative(1, -1, 0))) {
+            return center.getRelative(1, 0, 0).getLocation();
+        }
+        if (MinecartUtils.isTrack(center.getRelative(0, 0, 1))) {
+            return center.getRelative(0, 0, 1).getLocation();
+        }
+        else if (MinecartUtils.isTrack(center.getRelative(0, -1, 1)) && MinecartUtils.isSlopedTrack(center.getRelative(0, -1, 1))) {
+            return center.getRelative(0, 0, 1).getLocation();
+        }
+        return null;
+    }
+    
+    public static boolean doMinecartCollection(MinecartManiaMinecart minecart) {
+        if (minecart.getBlockTypeAhead() != null) {
+            if (minecart.getBlockTypeAhead().getType().getId() == Item.CHEST.getId()) {
+                MinecartManiaChest chest = MinecartManiaWorld.getMinecartManiaChest((Chest)minecart.getBlockTypeAhead().getState());
+                
+                if (SignCommands.isNoCollection(chest)) {
+                    return false;
+                }
+                
+                if (minecart instanceof MinecartManiaStorageCart) {
+                    MinecartManiaStorageCart storageCart = (MinecartManiaStorageCart)minecart;
+                    boolean failed = false;
+                    for (ItemStack item : storageCart.getInventory().getContents()) {
+                        if (!chest.addItem(item)) {
+                            failed = true;
+                            break;
+                        }
+                    }
+                    if (!failed) {
+                        storageCart.getInventory().clear();
+                    }
+                }
+                if (chest.addItem(minecart.getType().getId())) {
+                    
+                    minecart.kill(false);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public static boolean doCollectParallel(MinecartManiaMinecart minecart) {
+        ArrayList<Block> blockList = minecart.getParallelBlocks();
+        for (Block block : blockList) {
+            if (block.getState() instanceof Chest) {
+                MinecartManiaChest chest = MinecartManiaWorld.getMinecartManiaChest((Chest)block.getState());
+                ArrayList<Sign> signList = SignUtils.getAdjacentMinecartManiaSignList(chest.getLocation(), 1);
+                for (Sign sign : signList) {
+                    for (int i = 0; i < sign.getNumLines(); i++) {
+                        if (sign.getLine(i).toLowerCase().contains("parallel")) {
+                            sign.setLine(i, "[Parallel]");
+                            if (!minecart.isMovingAway(block.getLocation())) {
+                                if (chest.addItem(minecart.getType().getId())) {
+                                    minecart.kill(false);
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    private static int getNumItemsInBlock(Item item) {
+        switch(item) {
+            case CLAY_BALL:
+            case SNOW_BALL:
+            case STRING:
+                return 4;
+            default: 
+                return 9;
+        }
+    }
+    
+    public static void doItemCompression(MinecartManiaStorageCart minecart) {
+        HashSet<Block> blockList = minecart.getAdjacentBlocks(minecart.getRange());
+        for (Block block : blockList) {
+            if (block.getTypeId() == Item.WORKBENCH.getId()) {
+                ArrayList<Sign> signList = SignUtils.getAdjacentMinecartManiaSignList(block.getLocation(), 2);
+                for (Sign sign : signList) {
+                    for (int i = 0; i < sign.getNumLines(); i++) {
+                        if (sign.getLine(i).toLowerCase().contains("compress items")
+                                || sign.getLine(i).toLowerCase().contains("compress")) { 
+                            sign.setLine(i, "[Compress Items]");
+                            //TODO handling for custom recipies?
+                            Item[][] compressable = { 
+                                    {Item.IRON_INGOT, Item.GOLD_INGOT, Item.LAPIS_LAZULI, Item.DIAMOND,       Item.CLAY_BALL, Item.SNOW_BALL}, 
+                                    {Item.IRON_BLOCK , Item.GOLD_BLOCK, Item.LAPIS_BLOCK, Item.DIAMOND_BLOCK, Item.CLAY,      Item.SNOW} 
+                            };
+                            int n = 0;
+                            for (Item m : compressable[0]) {
+                                int amtPerBlock=getNumItemsInBlock(m);
+                                int amt = 0;
+                                int slot = 0;
+                                for (ItemStack item : minecart.getContents()) {
+                                    if (item != null && m.equals(Item.getItem(item))) {
+                                        amt += item.getAmount();
+                                        minecart.setItem(slot, null);
+                                    }
+                                    slot++;
+                                }
+                                int compressedAmt = amt / amtPerBlock;
+                                int left = amt % amtPerBlock;
+                                while (compressedAmt > 0) {
+                                    minecart.addItem(compressable[1][n].getId(), Math.min(64, compressedAmt));
+                                    compressedAmt -= Math.min(64, compressedAmt);
+                                }
+                                if (left > 0) {
+                                    ItemStack item = compressable[0][n].toItemStack();
+                                    item.setAmount(left);
+                                    minecart.addItem(item);
+                                }
+                                
+                                n++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    /*	
 	public static boolean doEmptyChestInventory(MinecartManiaStorageCart minecart) {
 		ArrayList<Sign> signList = SignUtils.getAdjacentSignList(minecart, 2);
 		for (Sign sign : signList) {
@@ -216,5 +232,5 @@ public abstract class ChestStorage {
 			}
 		}
 	}
-*/
+     */
 }
