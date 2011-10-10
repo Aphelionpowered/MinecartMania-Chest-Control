db6d317e41cccc86dbaa315524117b5e81ebd9c3
N3X15 <nexis@7chan.org>
2011-10-10 03:06:22 -0700

    formatting

--- src/com/afforess/minecartmaniachestcontrol/ChestStorage.java
+++ src/com/afforess/minecartmaniachestcontrol/ChestStorage.java
@@ -34,26 +34,22 @@ public abstract class ChestStorage {
     private static Location getAdjacentTrack(Block center) {
         if (MinecartUtils.isTrack(center.getRelative(-1, 0, 0))) {
             return center.getRelative(-1, 0, 0).getLocation();
-        }
-        else if (MinecartUtils.isTrack(center.getRelative(-1, -1, 0)) && MinecartUtils.isSlopedTrack(center.getRelative(-1, -1, 0))) {
+        } else if (MinecartUtils.isTrack(center.getRelative(-1, -1, 0)) && MinecartUtils.isSlopedTrack(center.getRelative(-1, -1, 0))) {
             return center.getRelative(-1, 0, 0).getLocation();
         }
         if (MinecartUtils.isTrack(center.getRelative(0, 0, -1))) {
             return center.getRelative(0, 0, -1).getLocation();
-        }
-        else if (MinecartUtils.isTrack(center.getRelative(0, -1, -1)) && MinecartUtils.isSlopedTrack(center.getRelative(0, -1, -1))) {
+        } else if (MinecartUtils.isTrack(center.getRelative(0, -1, -1)) && MinecartUtils.isSlopedTrack(center.getRelative(0, -1, -1))) {
             return center.getRelative(0, 0, -1).getLocation();
         }
         if (MinecartUtils.isTrack(center.getRelative(1, 0, 0))) {
             return center.getRelative(1, 0, 0).getLocation();
-        }
-        else if (MinecartUtils.isTrack(center.getRelative(1, -1, 0)) && MinecartUtils.isSlopedTrack(center.getRelative(1, -1, 0))) {
+        } else if (MinecartUtils.isTrack(center.getRelative(1, -1, 0)) && MinecartUtils.isSlopedTrack(center.getRelative(1, -1, 0))) {
             return center.getRelative(1, 0, 0).getLocation();
         }
         if (MinecartUtils.isTrack(center.getRelative(0, 0, 1))) {
             return center.getRelative(0, 0, 1).getLocation();
-        }
-        else if (MinecartUtils.isTrack(center.getRelative(0, -1, 1)) && MinecartUtils.isSlopedTrack(center.getRelative(0, -1, 1))) {
+        } else if (MinecartUtils.isTrack(center.getRelative(0, -1, 1)) && MinecartUtils.isSlopedTrack(center.getRelative(0, -1, 1))) {
             return center.getRelative(0, 0, 1).getLocation();
         }
         return null;
@@ -62,14 +58,14 @@ public abstract class ChestStorage {
     public static boolean doMinecartCollection(MinecartManiaMinecart minecart) {
         if (minecart.getBlockTypeAhead() != null) {
             if (minecart.getBlockTypeAhead().getType().getId() == Item.CHEST.getId()) {
-                MinecartManiaChest chest = MinecartManiaWorld.getMinecartManiaChest((Chest)minecart.getBlockTypeAhead().getState());
+                MinecartManiaChest chest = MinecartManiaWorld.getMinecartManiaChest((Chest) minecart.getBlockTypeAhead().getState());
                 
                 if (SignCommands.isNoCollection(chest)) {
                     return false;
                 }
                 
                 if (minecart instanceof MinecartManiaStorageCart) {
-                    MinecartManiaStorageCart storageCart = (MinecartManiaStorageCart)minecart;
+                    MinecartManiaStorageCart storageCart = (MinecartManiaStorageCart) minecart;
                     boolean failed = false;
                     for (ItemStack item : storageCart.getInventory().getContents()) {
                         if (!chest.addItem(item)) {
@@ -95,7 +91,7 @@ public abstract class ChestStorage {
         ArrayList<Block> blockList = minecart.getParallelBlocks();
         for (Block block : blockList) {
             if (block.getState() instanceof Chest) {
-                MinecartManiaChest chest = MinecartManiaWorld.getMinecartManiaChest((Chest)block.getState());
+                MinecartManiaChest chest = MinecartManiaWorld.getMinecartManiaChest((Chest) block.getState());
                 ArrayList<Sign> signList = SignUtils.getAdjacentMinecartManiaSignList(chest.getLocation(), 1);
                 for (Sign sign : signList) {
                     for (int i = 0; i < sign.getNumLines(); i++) {
@@ -116,16 +112,16 @@ public abstract class ChestStorage {
     }
     
     private static int getNumItemsInBlock(Item item) {
-        switch(item) {
+        switch (item) {
             case CLAY_BALL:
             case SNOW_BALL:
             case STRING:
                 return 4;
-            default: 
+            default:
                 return 9;
         }
     }
-
+    
     public static void doCrafting(MinecartManiaStorageCart minecart) {
         HashSet<Block> blockList = minecart.getAdjacentBlocks(minecart.getRange());
         for (Block block : blockList) {
@@ -137,56 +133,57 @@ public abstract class ChestStorage {
                         // For each line on the sign
                         String itemListString = "";
                         for (int i = 1; i < sign.getNumLines(); i++) {
-                            if(i>1) itemListString+=":";
-                            itemListString+=sign.getLine(i);
+                            if (i > 1)
+                                itemListString += ":";
+                            itemListString += sign.getLine(i);
                         }
-                        for(AbstractItem item:ItemUtils.getItemStringListToMaterial(itemListString.split(":"))) {
+                        for (AbstractItem item : ItemUtils.getItemStringListToMaterial(itemListString.split(":"))) {
                             // Get the recipe, if possible
                             RecipeData recipe = RecipeManager.findRecipe(item);
                             
-                            if(recipe==null) continue; // Skip if we can't find it.
-                            if(recipe.ingredients == null || recipe.ingredients.size()==0) continue;
+                            if (recipe == null)
+                                continue; // Skip if we can't find it.
+                            if (recipe.ingredients == null || recipe.ingredients.size() == 0)
+                                continue;
                             
-                            boolean outOfIngredients=false;
+                            boolean outOfIngredients = false;
                             
                             int loops = 0;
                             
                             //System.out.println("RECIPE: "+recipe.results.toString());
                             // Until we're out of ingredients, or the loop has been executed 64 times.
-                            while(!outOfIngredients && loops<64) {
-
-                                
-                                
+                            while (!outOfIngredients && loops < 64) {
                                 loops++;
                                 // Loop through the list of ingredients for this recipe
-                                for(ItemStack stack : recipe.ingredients) {
+                                for (ItemStack stack : recipe.ingredients) {
                                     // See if we have the needed ingredient
                                     Item sitem = Item.getItem(stack);
-                                    if(sitem==null) {
-                                        System.out.println("Could not find item for "+stack.toString());
-                                        outOfIngredients=true;
+                                    if (sitem == null) {
+                                        System.out.println("Could not find item for " + stack.toString());
+                                        outOfIngredients = true;
                                         break;
                                     }
-                                    if(minecart.amount(sitem)<stack.getAmount()) {
+                                    if (minecart.amount(sitem) < stack.getAmount()) {
                                         // Otherwise, break out of the loop.
-                                        outOfIngredients=true;
+                                        outOfIngredients = true;
                                         //System.out.println("OOI: "+stack.toString());
                                         break;
                                     }
                                 }
                                 
-                                if(outOfIngredients) break;
+                                if (outOfIngredients)
+                                    break;
                                 
-                                if(!minecart.canAddItem(recipe.results)) {
+                                if (!minecart.canAddItem(recipe.results)) {
                                     //System.out.println("CAI: "+recipe.results.toString());
-                                    outOfIngredients=true;
+                                    outOfIngredients = true;
                                     break;
                                 }
                                 
                                 // Loop through again to actually remove the items
-                                for(ItemStack stack : recipe.ingredients) {
+                                for (ItemStack stack : recipe.ingredients) {
                                     //System.out.println("[Craft Items] Removed "+stack.toString()+" from minecart!");
-                                    minecart.removeItem(stack.getTypeId(),stack.getAmount(),stack.getDurability());
+                                    minecart.removeItem(stack.getTypeId(), stack.getAmount(), stack.getDurability());
                                 }
                                 // Take it from the cart
                                 minecart.addItem(recipe.results);
@@ -198,7 +195,7 @@ public abstract class ChestStorage {
             }
         }
     }
-
+    
     public static void doItemCompression(MinecartManiaStorageCart minecart) {
         HashSet<Block> blockList = minecart.getAdjacentBlocks(minecart.getRange());
         for (Block block : blockList) {
@@ -206,17 +203,13 @@ public abstract class ChestStorage {
                 ArrayList<Sign> signList = SignUtils.getAdjacentMinecartManiaSignList(block.getLocation(), 2);
                 for (Sign sign : signList) {
                     for (int i = 0; i < sign.getNumLines(); i++) {
-                        if (sign.getLine(i).toLowerCase().contains("compress items")
-                                || sign.getLine(i).toLowerCase().contains("compress")) { 
+                        if (sign.getLine(i).toLowerCase().contains("compress items") || sign.getLine(i).toLowerCase().contains("compress")) {
                             sign.setLine(i, "[Compress Items]");
                             //TODO handling for custom recipies?
-                            Item[][] compressable = { 
-                                    {Item.IRON_INGOT, Item.GOLD_INGOT, Item.LAPIS_LAZULI, Item.DIAMOND,       Item.CLAY_BALL, Item.SNOW_BALL}, 
-                                    {Item.IRON_BLOCK , Item.GOLD_BLOCK, Item.LAPIS_BLOCK, Item.DIAMOND_BLOCK, Item.CLAY,      Item.SNOW_BLOCK} 
-                            };
+                            Item[][] compressable = { { Item.IRON_INGOT, Item.GOLD_INGOT, Item.LAPIS_LAZULI, Item.DIAMOND, Item.CLAY_BALL, Item.SNOW_BALL }, { Item.IRON_BLOCK, Item.GOLD_BLOCK, Item.LAPIS_BLOCK, Item.DIAMOND_BLOCK, Item.CLAY, Item.SNOW_BLOCK } };
                             int n = 0;
                             for (Item m : compressable[0]) {
-                                int amtPerBlock=getNumItemsInBlock(m);
+                                int amtPerBlock = getNumItemsInBlock(m);
                                 int amt = 0;
                                 int slot = 0;
                                 for (ItemStack item : minecart.getContents()) {
@@ -246,67 +239,11 @@ public abstract class ChestStorage {
             }
         }
     }
-    /*	
-	public static boolean doEmptyChestInventory(MinecartManiaStorageCart minecart) {
-		ArrayList<Sign> signList = SignUtils.getAdjacentSignList(minecart, 2);
-		for (Sign sign : signList) {
-			if (sign.getLine(0).toLowerCase().contains("trash items")) {
-				//return InventoryUtils.doInventoryTransaction(minecart, null, sign, minecart.getDirectionOfMotion());
-			}
-		}
-		return false;
-	}
-
-	public static void setMaximumItems(MinecartManiaStorageCart minecart) {
-		ArrayList<Sign> signList = SignUtils.getAdjacentSignList(minecart, 2);
-		for (Sign sign : signList) {
-			if (sign.getLine(0).toLowerCase().contains("max items")) {
-				String[] list = {sign.getLine(1), sign.getLine(2), sign.getLine(3) };
-				AbstractItem[] items = ItemUtils.getItemStringListToMaterial(list);
-				for (AbstractItem item : items) {
-					if (!item.isInfinite()) {
-						minecart.setMaximumItem(item.type(), item.getAmount());
-					}
-				}
-				sign.setLine(0, "[Max Items]");
-				if (!sign.getLine(1).isEmpty()) {
-					sign.setLine(1, StringUtils.addBrackets(sign.getLine(1)));
-				}
-				if (!sign.getLine(2).isEmpty()) {
-					sign.setLine(2, StringUtils.addBrackets(sign.getLine(2)));
-				}
-				if (!sign.getLine(3).isEmpty()) {
-					sign.setLine(3, StringUtils.addBrackets(sign.getLine(3)));
-				}
-				sign.update();
-			}
-		}
-	}
-	
-	public static void setMinimumItems(MinecartManiaStorageCart minecart) {
-		ArrayList<Sign> signList = SignUtils.getAdjacentSignList(minecart, 2);
-		for (Sign sign : signList) {
-			if (sign.getLine(0).toLowerCase().contains("min items")) {
-				String[] list = {sign.getLine(1), sign.getLine(2), sign.getLine(3) };
-				AbstractItem[] items = ItemUtils.getItemStringListToMaterial(list);
-				for (AbstractItem item : items) {
-					if (!item.isInfinite()) {
-						minecart.setMinimumItem(item.type(), item.getAmount());
-					}
-				}
-				sign.setLine(0, "[Min Items]");
-				if (!sign.getLine(1).isEmpty()) {
-					sign.setLine(1, StringUtils.addBrackets(sign.getLine(1)));
-				}
-				if (!sign.getLine(2).isEmpty()) {
-					sign.setLine(2, StringUtils.addBrackets(sign.getLine(2)));
-				}
-				if (!sign.getLine(3).isEmpty()) {
-					sign.setLine(3, StringUtils.addBrackets(sign.getLine(3)));
-				}
-				sign.update();
-			}
-		}
-	}
+    /*
+     * public static boolean doEmptyChestInventory(MinecartManiaStorageCart minecart) { ArrayList<Sign> signList = SignUtils.getAdjacentSignList(minecart, 2); for (Sign sign : signList) { if (sign.getLine(0).toLowerCase().contains("trash items")) { //return InventoryUtils.doInventoryTransaction(minecart, null, sign, minecart.getDirectionOfMotion()); } } return false; }
+     * 
+     * public static void setMaximumItems(MinecartManiaStorageCart minecart) { ArrayList<Sign> signList = SignUtils.getAdjacentSignList(minecart, 2); for (Sign sign : signList) { if (sign.getLine(0).toLowerCase().contains("max items")) { String[] list = {sign.getLine(1), sign.getLine(2), sign.getLine(3) }; AbstractItem[] items = ItemUtils.getItemStringListToMaterial(list); for (AbstractItem item : items) { if (!item.isInfinite()) { minecart.setMaximumItem(item.type(), item.getAmount()); } } sign.setLine(0, "[Max Items]"); if (!sign.getLine(1).isEmpty()) { sign.setLine(1, StringUtils.addBrackets(sign.getLine(1))); } if (!sign.getLine(2).isEmpty()) { sign.setLine(2, StringUtils.addBrackets(sign.getLine(2))); } if (!sign.getLine(3).isEmpty()) { sign.setLine(3, StringUtils.addBrackets(sign.getLine(3))); } sign.update(); } } }
+     * 
+     * public static void setMinimumItems(MinecartManiaStorageCart minecart) { ArrayList<Sign> signList = SignUtils.getAdjacentSignList(minecart, 2); for (Sign sign : signList) { if (sign.getLine(0).toLowerCase().contains("min items")) { String[] list = {sign.getLine(1), sign.getLine(2), sign.getLine(3) }; AbstractItem[] items = ItemUtils.getItemStringListToMaterial(list); for (AbstractItem item : items) { if (!item.isInfinite()) { minecart.setMinimumItem(item.type(), item.getAmount()); } } sign.setLine(0, "[Min Items]"); if (!sign.getLine(1).isEmpty()) { sign.setLine(1, StringUtils.addBrackets(sign.getLine(1))); } if (!sign.getLine(2).isEmpty()) { sign.setLine(2, StringUtils.addBrackets(sign.getLine(2))); } if (!sign.getLine(3).isEmpty()) { sign.setLine(3, StringUtils.addBrackets(sign.getLine(3))); } sign.update(); } } }
      */
 }
