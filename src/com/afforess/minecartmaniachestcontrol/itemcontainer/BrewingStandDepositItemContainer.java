3bd4fbaab9db8cdbf6e20ccf6065403bd13611ca
N3X15 <nexis@7chan.org>
2011-12-07 03:07:06 -0800

    Debugging stuff

--- src/com/afforess/minecartmaniachestcontrol/itemcontainer/BrewingStandDepositItemContainer.java
+++ src/com/afforess/minecartmaniachestcontrol/itemcontainer/BrewingStandDepositItemContainer.java
@@ -1,5 +1,6 @@
 package com.afforess.minecartmaniachestcontrol.itemcontainer;
 
+import com.afforess.minecartmaniacore.debug.MinecartManiaLogger;
 import com.afforess.minecartmaniacore.inventory.MinecartManiaBrewingStand;
 import com.afforess.minecartmaniacore.inventory.MinecartManiaInventory;
 import com.afforess.minecartmaniacore.utils.DirectionUtils.CompassDirection;
@@ -7,46 +8,51 @@ import com.afforess.minecartmaniacore.world.AbstractItem;
 import com.afforess.minecartmaniacore.world.Item;
 
 public class BrewingStandDepositItemContainer extends GenericItemContainer
-		implements ItemContainer {
-
-	private MinecartManiaBrewingStand brewingStand;
-
-	public BrewingStandDepositItemContainer(MinecartManiaBrewingStand brewingStand, String line,
-			CompassDirection direction) {
-		super(line, direction);
-		this.brewingStand=brewingStand;
-	}
-
-	public void doCollection(MinecartManiaInventory deposit) {
-		for (CompassDirection direction : directions) {
-			AbstractItem[] list = getItemList(direction);
-			for (AbstractItem item : list) {
-				if (item != null) {
-					short data = (short) (item.hasData() ? item.getData() : -1);
-					for(int slotId=0;slotId<3;slotId++) {
-						//does not match the item already in the slotId, continue
-						if (brewingStand.getItem(slotId) == null || !item.equals(Item.getItem(brewingStand.getItem(slotId)))) {
-							continue;
-						}
-						int toRemove = brewingStand.getItem(slotId).getAmount();
-						if (!item.isInfinite() && item.getAmount() < toRemove) {
-							toRemove = item.getAmount();
-						}
-						item.setAmount(toRemove);
-						if (brewingStand.canRemoveItem(item.getId(), toRemove, data)) {
-							if (deposit.canAddItem(item.toItemStack())) {
-								if (deposit.addItem(item.toItemStack())) {
-									brewingStand.setItem(slotId, null);
-								}
-								else {
-									return;
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
+        implements ItemContainer {
+    
+    private MinecartManiaBrewingStand brewingStand;
+    
+    public BrewingStandDepositItemContainer(
+            MinecartManiaBrewingStand brewingStand, String line,
+            CompassDirection direction) {
+        super(line, direction);
+        this.brewingStand = brewingStand;
+    }
+    
+    public void doCollection(MinecartManiaInventory deposit) {
+        for (CompassDirection direction : directions) {
+            AbstractItem[] list = getItemList(direction);
+            for (AbstractItem item : list) {
+                if (item != null) {
+                    short data = (short) (item.hasData() ? item.getData() : -1);
+                    for (int slotId = 0; slotId < 3; slotId++) {
+                        //does not match the item already in the slotId, continue
+                        if (brewingStand.getItem(slotId) == null || !item.equals(Item.getItem(brewingStand.getItem(slotId)))) {
+                            continue;
+                        }
+                        int toRemove = brewingStand.getItem(slotId).getAmount();
+                        if (!item.isInfinite() && item.getAmount() < toRemove) {
+                            toRemove = item.getAmount();
+                        }
+                        item.setAmount(toRemove);
+                        if (brewingStand.canRemoveItem(item.getId(), toRemove, data)) {
+                            if (deposit.canAddItem(item.toItemStack())) {
+                                if (deposit.addItem(item.toItemStack())) {
+                                    brewingStand.setItem(slotId, null);
+                                } else {
+                                    MinecartManiaLogger.getInstance().info("[BrewingStand:Deposit] Can't add " + item.toItemStack().toString());
+                                    return;
+                                }
+                            } else {
+                                MinecartManiaLogger.getInstance().info("[BrewingStand:Deposit] Can't deposit " + item.toItemStack().toString());
+                            }
+                        } else {
+                            MinecartManiaLogger.getInstance().info("[BrewingStand:Deposit] Can't remove " + item.toItemStack().toString());
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
 }
