cc175556678d5498b07507f592a5cc3f4addcd7e
N3X15 <nexis@7chan.org>
2011-12-11 04:47:29 -0800

    Fix another potential problem area

--- src/com/afforess/minecartmaniachestcontrol/itemcontainer/BrewingStandBottomContainer.java
+++ src/com/afforess/minecartmaniachestcontrol/itemcontainer/BrewingStandBottomContainer.java
@@ -5,6 +5,7 @@ import org.bukkit.inventory.ItemStack;
 import com.afforess.minecartmaniacore.inventory.MinecartManiaBrewingStand;
 import com.afforess.minecartmaniacore.inventory.MinecartManiaInventory;
 import com.afforess.minecartmaniacore.utils.DirectionUtils.CompassDirection;
+import com.afforess.minecartmaniacore.utils.ItemMatcher;
 import com.afforess.minecartmaniacore.world.AbstractItem;
 import com.afforess.minecartmaniacore.world.SpecificMaterial;
 
@@ -33,32 +34,34 @@ public class BrewingStandBottomContainer extends GenericItemContainer implements
         ItemStack[] cartContents = withdraw.getContents();
         ItemStack[] standContents = brewingStand.getContents();
         for (CompassDirection direction : directions) {
-            SpecificMaterial[] list = this.getItemList(direction);
-            for (SpecificMaterial item : list) {
+            for (ItemStack item : cartContents) {
                 if (item != null) {
-                    for (int i = 0; i < 3; i++) {
-                        ItemStack slotContents = brewingStand.getItem(i);
-                        
-                        // Ensure the slot is clear. (No stacking allowed)
-                        if (slotContents != null) {
-                            continue; // Skip it.
+                    for (ItemMatcher matcher : this.getMatchers(direction)) {
+                        if (matcher.match(item)) {
+                            for (int i = 0; i < 3; i++) {
+                                ItemStack slotContents = brewingStand.getItem(i);
+                                
+                                // Ensure the slot is clear. (No stacking allowed)
+                                if (slotContents != null) {
+                                    continue; // Skip it.
+                                }
+                                
+                                // Try to remove the item from the cart.
+                                if (!withdraw.removeItem(item.getTypeId(), 1, (short) item.getDurability())) {
+                                    //Failed, restore backup of inventory
+                                    withdraw.setContents(cartContents);
+                                    brewingStand.setContents(standContents);
+                                    return;
+                                }
+                                
+                                // Awesome, add it to the stand.
+                                brewingStand.setItem(i, new ItemStack(item.getTypeId(), 1, (short) item.getDurability()));
+                                
+                            }
                         }
-                        
-                        // Try to remove the item from the cart.
-                        if (!withdraw.removeItem(item.getId(), 1, (short) item.getData())) {
-                            //Failed, restore backup of inventory
-                            withdraw.setContents(cartContents);
-                            brewingStand.setContents(standContents);
-                            return;
-                        }
-                        
-                        // Awesome, add it to the stand.
-                        brewingStand.setItem(i, new ItemStack(item.getId(), 1, (short) item.getData()));
-                        
                     }
                 }
             }
         }
     }
-    
 }
