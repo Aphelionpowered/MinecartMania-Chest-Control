8081fbdcddb75c352447348ee29b4983c04acee7
N3X15 <nexis@7chan.org>
2011-12-11 04:50:20 -0800

    Fix another NPE.  Almost there.

--- src/com/afforess/minecartmaniachestcontrol/itemcontainer/ItemDepositContainer.java
+++ src/com/afforess/minecartmaniachestcontrol/itemcontainer/ItemDepositContainer.java
@@ -30,16 +30,18 @@ public class ItemDepositContainer extends GenericItemContainer implements
                     int amount = matcher.getAmount();
                     for (int i = 0; i < inventory.size(); i++) {
                         ItemStack itemStack = inventory.getItem(i);
-                        itemStack.setAmount(amount);
-                        if (matcher.match(itemStack) && amount > 0) {
-                            int toAdd = (itemStack.getAmount() > amount ? amount : itemStack.getAmount());
-                            if (!inventory.canRemoveItem(itemStack.getTypeId(), toAdd, itemStack.getDurability())) {
-                                break; //if we are not allowed to remove the items, give up
-                            } else if (!deposit.addItem(new ItemStack(itemStack.getTypeId(), toAdd, itemStack.getDurability()))) {
-                                break;
+                        if (itemStack != null) {
+                            itemStack.setAmount(amount);
+                            if (matcher.match(itemStack) && amount > 0) {
+                                int toAdd = (itemStack.getAmount() > amount ? amount : itemStack.getAmount());
+                                if (!inventory.canRemoveItem(itemStack.getTypeId(), toAdd, itemStack.getDurability())) {
+                                    break; //if we are not allowed to remove the items, give up
+                                } else if (!deposit.addItem(new ItemStack(itemStack.getTypeId(), toAdd, itemStack.getDurability()))) {
+                                    break;
+                                }
+                                inventory.removeItem(itemStack.getTypeId(), toAdd, itemStack.getDurability());
+                                amount -= toAdd;
                             }
-                            inventory.removeItem(itemStack.getTypeId(), toAdd, itemStack.getDurability());
-                            amount -= toAdd;
                         }
                     }
                 }
