1d560494c6f92270e8d84c3285bc9f0e12d52d6a
N3X15 <nexis@7chan.org>
2011-12-12 03:48:15 -0800

    Fix NPEs in Min/Max Items signs

--- src/com/afforess/minecartmaniachestcontrol/signs/MaximumItemAction.java
+++ src/com/afforess/minecartmaniachestcontrol/signs/MaximumItemAction.java
@@ -20,6 +20,7 @@ public class MaximumItemAction implements SignAction {
     public boolean execute(MinecartManiaMinecart minecart) {
         if (minecart.isStorageMinecart()) {
             for (ItemMatcher matcher : matchers) {
+                if(matcher==null) continue;
                 for (int i = 0; i < ((MinecartManiaStorageCart) minecart).size(); i++) {
                     ItemStack item = ((MinecartManiaStorageCart) minecart).getItem(i);
                     if(item==null) continue;
