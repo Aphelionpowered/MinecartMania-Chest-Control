e58fd9770bd07bc57e704ec827041dfe627cd034
N3X15 <nexis@7chan.org>
2011-12-22 12:35:34 -0800

    Complete re-code of Collect Items feature using mathematics instead of brute force.
    BONUS:  A fuck of a lot more efficient since it's not looping over everything.

--- src/com/afforess/minecartmaniachestcontrol/itemcontainer/ItemCollectionContainer.java
+++ src/com/afforess/minecartmaniachestcontrol/itemcontainer/ItemCollectionContainer.java
@@ -1,5 +1,6 @@
 package com.afforess.minecartmaniachestcontrol.itemcontainer;
 
+import org.bukkit.Material;
 import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 
@@ -17,53 +18,70 @@ public class ItemCollectionContainer extends GenericItemContainer implements Ite
     }
     
     public void doCollection(final MinecartManiaInventory withdraw) {
-        final ItemStack[] cartContents = withdraw.getContents();
-        final ItemStack[] chestContents = inventory.getContents();
+        final ItemStack[] cartContents = withdraw.getContents().clone();
+        final ItemStack[] chestContents = inventory.getContents().clone();
         for (final CompassDirection direction : directions) {
-            for (final ItemStack item : cartContents) {
-                if (item != null) {
-                    for (final ItemMatcher matcher : getMatchers(direction)) {
-                        if (matcher == null) {
-                            continue;
-                        }
-                        
-                        if (matcher.match(item)) {
-                            for (int i = 0; i < chestContents.length; i++) {
-                                final ItemStack slotContents = inventory.getItem(i);
-                                
-                                // Get the amount we want to add to the slot
-                                int amount = item.getAmount();
-                                
-                                // Get the maximum stack size (or just 64 if we've disabled that)
-                                int maxamount = MinecartManiaWorld.getMaxStackSize(item);
-                                
-                                // Non-empty slot
-                                if (slotContents != null) {
-                                    // Ensure we have the same ID and durability and enchantments
-                                    if (slotContents.getTypeId() == item.getTypeId() && slotContents.getDurability() == item.getDurability() && slotContents.getEnchantments() == item.getEnchantments()) {
-                                        
-                                        // Figure out how much we have to add to complete the stack.
-                                        amount = Math.min(amount, Math.max(0, maxamount - amount));
-                                        
-                                    } else {
-                                        // Skip it, not the same.
-                                        continue;
-                                    }
-                                }
-                                // Try to remove the item from the cart.
-                                if (!withdraw.removeItem(item.getTypeId(), amount, item.getDurability())) {
-                                    //Failed, restore backup of inventory
-                                    withdraw.setContents(cartContents);
-                                    inventory.setContents(chestContents);
-                                    return;
-                                }
-                                
-                                // Awesome, add it to the stand.
-                                inventory.setItem(i, new ItemStack(item.getTypeId(), amount, item.getDurability()));
-                            }
-                        }
+            for (final ItemMatcher matcher : getMatchers(direction)) {
+                if (matcher == null) {
+                    continue;
+                }
+                ItemStack item = null;
+                // Find the first matching item.
+                for (final ItemStack mySlot : withdraw.getContents().clone()) {
+                    if (matcher.match(mySlot)) {
+                        item = mySlot;
+                        break;
                     }
                 }
+                // Nothing matches
+                if (item == null)
+                    continue;
+                
+                // Get the maximum stack size (or just 64 if we've disabled that)
+                int maxamount = MinecartManiaWorld.getMaxStackSize(item);
+                
+                // Get the amount we want to add to the slot
+                int amount = Math.min(withdraw.amount(matcher), matcher.getAmount(Integer.MAX_VALUE));
+                
+                // Get the amount of available slots
+                int emptySlots = 0;
+                
+                // How much room is available in terms of incomplete stacks (<64)
+                int slack = 0;
+                
+                for (final ItemStack slot : inventory.getContents()) {
+                    // Non-empty slot
+                    if (slot != null && slot.getType() != Material.AIR) {
+                        // Ensure we have the same ID and durability and enchantments
+                        if (slot.getTypeId() == item.getTypeId() && slot.getDurability() == item.getDurability()) {
+                            //if the slot amount is negative, skip it.
+                            if (slot.getAmount() < 0)
+                                continue;
+                            
+                            // Figure out how much we have to add to complete the stack.
+                            slack += Math.min(maxamount, Math.max(0, maxamount - slot.getAmount()));
+                        } else {
+                            // Skip it, not the same.
+                            continue;
+                        }
+                    } else {
+                        // Empty slot, count it.
+                        emptySlots++;
+                    }
+                }
+                // And finally, add up the number of empty slots (times stack size) and how much slack we have.
+                amount = (emptySlots * maxamount) + slack;
+                // Try to remove the items from the cart.
+                if (withdraw.removeItem(item.getTypeId(), amount, item.getDurability())) {
+                    // Awesome, add it to the stand.
+                    if (inventory.addItem(new ItemStack(item.getTypeId(), amount, item.getDurability()))) {
+                        continue;
+                    }
+                }
+                //Failed, restore backup of inventory
+                withdraw.setContents(cartContents);
+                inventory.setContents(chestContents);
+                return;
             }
         }
     }
